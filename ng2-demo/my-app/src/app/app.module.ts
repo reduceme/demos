d3ec9a39d3c15cd8a78b797b670c7cf249cd7f10
regdai <1148440350@qq.com>
2017-10-30 16:01:14 +0800

    from

--- ng2-demo/my-app/src/app/app.module.ts
+++ ng2-demo/my-app/src/app/app.module.ts
@@ -1,17 +1,33 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {DashboardComponent} from './dashboard.component';
+import {HeroDetailComponent} from './hero-detail.component';
+import {HeroesComponent} from './heroes.component';
+import {HeroService} from './hero.service';
+
+import {AppRoutingModule} from './app-routing.module';
 
 @NgModule({
+  //本模块声明的组件模板需要的类所在的其它模块。
   imports: [
     BrowserModule,
-    FormsModule // <-- import the FormsModule before binding with [(ngModel)]
+    FormsModule,
+    AppRoutingModule
   ],
+  //声明本模块中拥有的视图类。Angular 有三种视图类：组件、指令和管道。
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    HeroDetailComponent,
+    HeroesComponent
   ],
-  bootstrap: [ AppComponent ]
+  //服务的创建者，并加入到全局服务列表中，可用于应用任何部分。
+  providers: [HeroService],
+  //指定应用的主视图（称为根组件），它是所有其它视图的宿主。只有根模块才能设置bootstrap属性。
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
