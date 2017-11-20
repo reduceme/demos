2fa1c143904140efba97542cd4c71dbbe7701611
regdai <1148440350@qq.com>
2017-11-20 11:06:39 +0800

    删除了一些文件

--- ts_demo/hello-world/helloWorld.ts
+++ ts_demo/hello-world/helloWorld.ts
@@ -1,23 +1,3 @@
-/*function greeter(person: string) {
- return "Hello, " + person;
- }
-
- var user = "Jane User";
- document.body.innerHTML = greeter(user);*/
-
-//接口
-/*interface Person {
-    firstName: string;
-    lastName: string;
-}
-
-function greeter(person: Person) {
-    return "Hello," + person.firstName + " " + person.lastName;
-}
-
-var user = {firstName: "Jane", lastName: "User"};
-document.body.innerHTML = greeter(user);*/
-
 //类
 class Student {
     fullName: string;
