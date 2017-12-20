9c61f410fa7f75f622c63be8d5545586d0195c78
regdai <1148440350@qq.com>
2017-12-20 16:26:39 +0800

    阅读到7

--- react_demo/14-chess/react-router-tutorial/lessons/05-active-links/modules/Repos.js
+++ react_demo/14-chess/react-router-tutorial/lessons/05-active-links/modules/Repos.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 export default React.createClass({
-  render() {
-    return <div>Repos</div>
-  }
+    render() {
+        return <div>Repos</div>
+    }
 })
--- react_demo/14-chess/react-router-tutorial/lessons/07-more-nesting/modules/Repo.js
+++ react_demo/14-chess/react-router-tutorial/lessons/07-more-nesting/modules/Repo.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
 export default React.createClass({
-  render() {
-    return (
-      <div>
-        <h2>{this.props.params.repoName}</h2>
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div>
+                <h2>{this.props.params.repoName}</h2>
+            </div>
+        )
+    }
 })
