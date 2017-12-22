ebfb6ee3fa436dd4a23b91307c2beff5a6757af5
regdai <1148440350@qq.com>
2017-12-22 14:03:02 +0800

    字符串分组方法

--- public/publicController.js
+++ public/publicController.js
@@ -226,3 +226,16 @@ function ajax(userInfo){
             }
         })
 }
+
+//字符串分组
+function splitStr(item, string) {
+        var arr = item.split(",");
+        var str = new RegExp(string);
+        var result = [];
+        for (var i = 0; i < arr.length; i++) {
+            if (str.test(arr[i])) {
+                result.push(arr[i]);
+            }
+        }
+        return result;
+    }
