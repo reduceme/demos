34e4d23fa8896e8b2ec795d915720e8bbc69df99
regdai <1148440350@qq.com>
2018-01-24 16:43:50 +0800

    heroForm

---
+++ ng2_demo/heroForm/src/app/hero-form.component.ts
@@ -0,0 +1,24 @@
+import {Component} from '@angular/core';
+
+import {Hero} from './hero';
+
+@Component({
+  selector: 'app-hero-form',
+  templateUrl: './hero-form.component.html'
+})
+
+export class HeroFormComponent {
+  powers = ['Really Smart', 'Super Flexible', 'Super Hot', 'Weather Changer'];
+
+  model = new Hero(18, 'Dr IQ', this.powers[0], 'Chunk Overstreet');
+  submitted = false;
+
+  onSubmit() {
+    this.submitted = true;
+  }
+
+//  TODO: Remove this when we're done
+  get diagonstic() {
+    return JSON.stringify(this.model);
+  }
+}
